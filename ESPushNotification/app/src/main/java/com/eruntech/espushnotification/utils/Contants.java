ac6adc6cbde667d8eb81979e62be65f2ca4696aa
杜明悦 <>
2018-03-07 17:28:29 +0800

    no message

--- ESPushNotification/app/src/main/java/com/eruntech/espushnotification/utils/Contants.java
+++ ESPushNotification/app/src/main/java/com/eruntech/espushnotification/utils/Contants.java
@@ -8,5 +8,5 @@ package com.eruntech.espushnotification.utils;
 public class Contants {
     public static final boolean DEBUG = true;
 
-    public static final String PACKAGE_NAME = "com.jiangdg.keepappalive";
+    public static final String PACKAGE_NAME = "com.greenbarracks";
 }
